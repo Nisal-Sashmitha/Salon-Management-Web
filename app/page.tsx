b1940d0ee6daa7140de7d089fed50fc61725b1f9
nisal <it20081034@my.sliit.lk>
2025-08-15 16:57:01 +0530

    chore: add image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AboutSection from "@/components/home/AboutSection";
 import Link from "next/link";
 import ContactUsSection from "@/components/home/ContactUsSection";
@@ -6,20 +7,24 @@ import ServicePreviewSection from "@/components/home/ServicePreviewSection";
 export default function Home() {
   return (
     <>
-      <div
-        className="relative w-full md:h-screen h-[70vh] bg-cover bg-center"
-        style={{ backgroundImage: "url('/assets/HomeHeaderImage.png')" }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="relative w-full md:h-screen h-[70vh] bg-cover bg-center overflow-hidden">
+        {/* Optimized background image */}
+        <Image
+          src="/assets/HomeHeaderImage.png"
+          alt="Salon Header"
+          fill
+          priority
+          className="object-cover z-0"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
 
-        <div className="flex flex-col items-center justify-center text-center text-white px-4 md:px-6 h-full">
-          <blockquote className="text-3xl md:text-5xl md:text-nowrap font-semibold italic max-w-2xl md:max-w-3xl mx-auto z-10">
+        <div className="flex flex-col items-center justify-center text-center text-white px-4 md:px-6 h-full relative z-20">
+          <blockquote className="text-3xl md:text-5xl md:text-nowrap font-semibold italic max-w-2xl md:max-w-3xl mx-auto">
             &quot;Capturing the Essence of You.&quot;
           </blockquote>
           <p className="mt-4 text-base md:text-xl text-gray-300">
             Ladies Hair and Beauty Salon.
           </p>
-          {/* bg-gradient-to-r from-pink-500 to-red-500 */}
           <Link href="/appointment">
             <button className="mt-8 px-6 py-3 md:px-9 md:py-3 bg-white text-black font-bold text-base md:text-lg rounded-full hover:shadow-lg hover:scale-105 transition transform">
               Book Now
@@ -27,9 +32,9 @@ export default function Home() {
           </Link>
         </div>
       </div>
-  <AboutSection />
-  <ServicePreviewSection />
-  <ContactUsSection />
+      <AboutSection />
+      <ServicePreviewSection />
+      <ContactUsSection />
     </>
   );
 }
